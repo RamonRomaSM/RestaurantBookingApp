56638c3472d5f3ef2f980bbdec470c3180d04c36
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2023-12-25 18:20:06 +0100

    Revert "a"
    This reverts commit 0708be5b3b3e18bb275edab7c704bda6c3f6e58c.

--- BooKing/.idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,9 +0,0 @@
-
-n
->app/src/main/java/com/example/booking/TiposComidaFragment.java,9\d\9d34362d63e28e03d53cf1dba65570a071f19220
-f
-6app/src/main/java/com/example/booking/TiposComida.java,6\0\606158c18c93a666285a5dadc5ce79db41a9bc28
-e
-5app/src/main/java/com/example/booking/TipoComida.java,e\7\e72fa8a8193be8d1fcbbea652c94515221809def
-g
-7app/src/main/java/com/example/booking/MainActivity.java,1\3\139978866d209e9fb039409827098ae40487bb62
--- BooKing/app/src/main/java/com/example/booking/MainActivity.java
+++ BooKing/app/src/main/java/com/example/booking/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.booking;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -12,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentContainerView contenedor=findViewById(R.id.contenedor);
-        TiposComidaFragment frag=new TiposComidaFragment();
-        FragmentManager fm =getSupportFragmentManager();
-
-
-        fm.beginTransaction().replace(contenedor.getId(),frag).commit();
     }
-
 }
