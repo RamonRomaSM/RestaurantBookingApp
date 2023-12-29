53811da4831c97203b13618d71f83fc04b5a194a
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2023-12-29 17:34:03 +0100

    a

--- Booking/app/src/main/java/com/example/booking/MenuFragment.java
+++ Booking/app/src/main/java/com/example/booking/MenuFragment.java
@@ -41,7 +41,7 @@ public class MenuFragment extends Fragment {
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tipos_comida, container, false);
+        return inflater.inflate(R.layout.fragment_menu, container, false);
     }
 
     @Override
