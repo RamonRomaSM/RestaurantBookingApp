0aa72669a37caf36c184f15259660fd2fe390541
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-02-13 01:49:50 +0100

    a

--- Booking/app/src/main/java/com/example/booking/HacerReservaFragment.java
+++ Booking/app/src/main/java/com/example/booking/HacerReservaFragment.java
@@ -9,7 +9,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HacerReservaFragment extends Fragment {
@@ -28,17 +34,35 @@ public class HacerReservaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hacer_reserva, container, false);
-        //TODO: aqui poner el contenido de los spinners, hacer metodo reservar
+
 
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        //TODO: aqui poner el contenido de los spinners, hacer metodo reservar
         Button volver=getView().findViewById(R.id.volver);
         volver.setOnClickListener(v -> {
             getParentFragmentManager().popBackStack();
         });
+
+
+        List<String> list = new ArrayList<String>();
+
+
+        Spinner horainicial=getView().findViewById(R.id.horaInicial);
+        Spinner horafinal=getView().findViewById(R.id.horaFinal);
+        for(int i=12;i<22;i++){
+            for(int j=0;i<4;j++){
+                list.add(i+":"+(15*j));
+            }
+
+        }
+        ArrayAdapter<String> adp1 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list);
+        adp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        horafinal.setAdapter(adp1);
+
+
     }
 }
